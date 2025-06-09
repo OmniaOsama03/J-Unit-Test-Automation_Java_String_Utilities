f2eae760048073f3c9ad012ccee5b7ff9b3adaed
OmniaOsama03 <Omniaosama432@gmail.com>
2025-06-10 00:14:12 +0400

    countVowels refactoring

--- Test Automation- JUnit/src/main/java/org/example/TextUtils.java
+++ Test Automation- JUnit/src/main/java/org/example/TextUtils.java
@@ -7,30 +7,25 @@ public class TextUtils
 {
 
     public static int countVowels(String s) {
-        if (s == null)
-        {
+        if (s == null) {
             return 0;
         }
 
         int count = 0;
 
-        for (int i = 0; i < s.length(); i++)
-        {
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-
-            //Checking lowercase vowels
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-            {
-                count++;
-
-                //Checking uppercase vowels
-            } else if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-            {
+            if (isVowel(ch)) {
                 count++;
             }
         }
 
         return count;
+    }
+
+    private static boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
     }
 
     public static String removePunctuation(String s)
